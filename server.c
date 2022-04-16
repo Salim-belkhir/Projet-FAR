fe31e652cbabb481c9c6c67dfa3730afc5dfac3a
Salim Belkhir <salim.belkhir@etu.umontpellier.fr>
2022-04-16 19:13:57 +0200

    Mise à jour des commentaires server.c

--- server.c
+++ server.c
@@ -10,34 +10,37 @@
 
 #define longueurMessage 256
 
+//tableau contenant les deux clients
 int connec[2] = {-1,-1};
 
+
+//fonction qui va être utilisée par un thread du serveur pour 
+//pouvoir transmettre les messages du client 1 au client 2
 void * Relayer1()
-{
-    
+{   //on ecrase d'abord les données contenues dans messageEnvoi et messageRecu pour éviter d'avoir des données non désirables
     char messageEnvoi[longueurMessage];
     char messageRecu[longueurMessage];
     int ecrits, lus; 
-    // On réception les données du client (cf. protocole)
+    // On réceptionne les données du client (cf. protocole)
     while(1)
     {
         memset(messageEnvoi, 0x00, longueurMessage*sizeof(char));
         memset(messageRecu, 0x00, longueurMessage*sizeof(char));
+        //On lit le message envoyé par le client
         lus = read(connec[0],messageRecu,longueurMessage*sizeof(char));
         switch(lus)
-        {
+        {   //en cas d'erreur dans la lecture, -1 sera retourné par "lus"
             case -1: 
-                perror("read");
-                
+                perror("[-]Problem receiving the message");               
                 close(connec[0]);
                 close(connec[1]);
                 exit(-5);
             case 0:
-                fprintf(stderr, "La socket a été fermée par le client !\n\n");
+                fprintf(stderr, "[!]The socket was closed by the client !\n\n");
                 close(connec[0]);
                 close(connec[1]);
             default:
-                printf("Message recu du client : %s (%d octets)\n\n",messageRecu,lus);
+                printf("Message receive by the client : %s (%d octets)\n\n",messageRecu,lus);
         }
 
         //On envoie des données vers le client (cf. protocole)    
@@ -46,12 +49,12 @@ void * Relayer1()
         switch(ecrits)
         {
             case -1: 
-                perror("write");
+                perror("[-]Problem of send the message");
                 close(connec[0]);
                 close(connec[1]);
                 exit(-6);
             case 0:
-                fprintf(stderr, "La socket a été fermée par le client !\n\n");
+                fprintf(stderr, "[!]The socket was closed by the client !\n\n");
                 close(connec[0]);
                 close(connec[1]);
             default:
@@ -61,27 +64,31 @@ void * Relayer1()
 }
 
 
+//fonction qui va être utilisée par un thread du serveur pour 
+//pouvoir transmettre les messages du client 2 au client 1 cette fois-ci
 void * Relayer2()
 {
     char messageEnvoi[longueurMessage];
     char messageRecu[longueurMessage];
     int ecrits, lus; 
     
-    // On réception les données du client (cf. protocole)
+    // On réceptionne les données du client (cf. protocole)
     while(1)
-    {
+    {   //on ecrase d'abord les données contenues dans messageEnvoi et messageRecu pour éviter d'avoir des données non désirables
         memset(messageEnvoi, 0x00, longueurMessage*sizeof(char));
         memset(messageRecu, 0x00, longueurMessage*sizeof(char));
+        //On lit le message envoyé par le client
         lus = read(connec[1],messageRecu,longueurMessage*sizeof(char));
         switch(lus)
         {
+            //en cas d'erreur dans la lecture, -1 sera retourné par "lus"
             case -1: 
-                perror("read");
+                perror("[-]Problem receiving the message");
                 close(connec[0]);
                 close(connec[1]);
                 exit(-5);
             case 0:
-                fprintf(stderr, "La socket a été fermée par le client !\n\n");
+                fprintf(stderr, "[!]The socket was closed by the client !\n\n");
                 close(connec[0]);
                 close(connec[1]);
             default:
@@ -100,7 +107,7 @@ void * Relayer2()
                 close(connec[1]);
                 exit(-6);
             case 0:
-                fprintf(stderr, "La socket a été fermée par le client !\n\n");
+                fprintf(stderr, "[!]The socket was closed by the client !\n\n");
                 close(connec[0]);
                 close(connec[1]);
             default:
@@ -122,10 +129,7 @@ int main(int argc, char *argv[])
     //  Creation des threads pour envoyer et recevoir des messages 
     pthread_t tRelay1;
     pthread_t tRelay2;
-    
-    
-    //POUR SAVOIR SI LES DEUX CLIENTS SONT CONNECTE
-    //int connect[2] = {-1,-1};
+
 
     // Création d'un socket de communication
     // PF_INET c'est le domaine pour le protocole internet IPV4 
@@ -138,13 +142,14 @@ int main(int argc, char *argv[])
     // afin de vérifier la bonne création de cela
     if(socketServeur < 0)
     {
-        perror("socket not created");
+        perror("[-]Socket not created");
         exit(-1); // On sort en indiquant un code erreur
     }
-    printf("la Socket created successfully ! (%d)\n", socketServeur);
+    printf("[+]Socket created successfully ! (%d)\n", socketServeur);
     
     
     longueurAdresse = sizeof(pointDeRencontreLocal);
+
     // Initialise à 0 la struct sockaddr_in
     memset(&pointDeRencontreLocal, 0x00, longueurAdresse);
 
@@ -159,32 +164,34 @@ int main(int argc, char *argv[])
     // On demande l'attachement local de la socket 
     if((bind(socketServeur,(struct sockaddr *)&pointDeRencontreLocal, longueurAdresse)) < 0)
     {
-        perror("bind");
+        perror("[-]Problem of binding");
         exit(-2);
     }
 
-    printf("Socket attachée avec succés !\n");
+    printf("[+]Socket attached with success!\n");
 
-    // maximum 5 dans la fille
+    // maximum 5 clients dans la fille
     if(listen(socketServeur, 5) < 0)
     {
-        perror("listen");
+        perror("[-]The server can not listen");
         exit(-3);
     }
 
-    printf("Socket in listening! \n");
+    printf("Server on listening! \n");
     int i ;
     while(1){
-        // Boucle d'attente de connexion: en théorie, un  serveur attend indéfiniment à modifier avec des processus
+        //Boucle d'attente de connexion: en théorie, un serveur attend indéfiniment
         i = 0;
         while( i < 2)
         {
+            //Dans un premier temps, il faut s'assurer qu'on a bien deux clients qui vont se connecter
+            //on va d'abord rester dans cette boucle, tant que deux clients ne se sont pas bien connectés
             printf("Attente d'une demande de connexion (quitter avec Ctrl-C) \n\n");
             // c'est un appel bloquant 
             socketDialogue = accept(socketServeur, (struct sockaddr *)&pointDeRencontreDistant, &longueurAdresse);
             if(socketDialogue < 0)
             {
-                perror("accept");
+                perror("[-]We can not connect the client");
                 close(socketDialogue);
                 close(socketServeur);
                 exit(-4);
@@ -193,7 +200,7 @@ int main(int argc, char *argv[])
             connec[i] = socketDialogue;
             i++; 
         }
-        
+        //une fois qu'on a réussi à connecter les deux clients, on lance les deux threads qui vont relayer les messages
         pthread_create(&tRelay1, NULL, Relayer1, NULL); 
         pthread_create(&tRelay2, NULL, Relayer2, NULL);
         pthread_join(tRelay1, NULL);    
